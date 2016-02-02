ff1528b1571a611d0bc816af13f402eed10c719f
jbe2277 <jbe2277@users.noreply.github.com>
2016-02-02 22:16:49 +0100

    Move ViewModelTest unit test to Core

---
+++ src/WpfApplicationFramework/System.Waf/System.Waf.Core.Test/Applications/ViewModelCoreTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Waf.Applications;
+using System.Waf.UnitTesting;
+
+namespace Test.Waf.Applications
+{
+    [TestClass]
+    public class ViewModelCoreTest
+    {
+        [TestMethod]
+        public void GetViewAndVerifyDataContext()
+        {
+            IView view1 = new MockView();
+            MockViewModel viewModel1 = new MockViewModel(view1, false);
+            Assert.AreEqual(view1, viewModel1.View);
+            Assert.IsNull(view1.DataContext);
+
+            IView view2 = new MockView();
+            MockViewModel viewModel2 = new MockViewModel(view2, true);
+            Assert.AreEqual(view2, viewModel2.View);
+            Assert.AreEqual(viewModel2, view2.DataContext);
+        }
+
+        [TestMethod]
+        public void ConstructorParameter()
+        {
+            AssertHelper.ExpectedException<ArgumentNullException>(() => new MockViewModel(null, true));
+        }
+        
+
+        private class MockViewModel : ViewModelCore
+        {
+            public MockViewModel(IView view, bool initializeDataContext) : base(view, initializeDataContext)
+            {
+            }
+        }
+
+        private class MockView : IView
+        {
+            public object DataContext { get; set; }
+        }
+    }
+}
