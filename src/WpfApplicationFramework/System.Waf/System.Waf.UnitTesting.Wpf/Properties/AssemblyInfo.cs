079d4bc7dd4d65dcf8a70fbe684cafc898b1b232
jbe2277 <jbe2277@users.noreply.github.com>
2016-02-02 19:58:43 +0100

    Rename WpfApplicationFramework.UnitTesting.dll to System.Waf.UnitTesting.Wpf.dll

--- src/WpfApplicationFramework/System.Waf/System.Waf.UnitTesting.Wpf/Properties/AssemblyInfo.cs
+++ src/WpfApplicationFramework/System.Waf/System.Waf.UnitTesting.Wpf/Properties/AssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyTitle("WPF Application Framework (WAF) - UnitTesting Extension")]
+[assembly: AssemblyTitle("System.Waf.UnitTesting.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("WPF Application Framework (WAF)")]
+[assembly: AssemblyProduct("System.Waf.UnitTesting.Wpf")]
 
 
 [assembly: Guid("497d4744-3d62-432d-81d9-7d77c0590f05")]
