3e79c535cd49a1cff4af0d5eae26b77bc8c8d940
jbe2277 <jbe2277@users.noreply.github.com>
2016-02-06 11:19:21 +0100

    Add AsyncDelegateCommand class

--- src/WpfApplicationFramework/System.Waf/System.Waf.Core/Applications/AsyncDelegateCommand.cs
+++ src/WpfApplicationFramework/System.Waf/System.Waf.Core/Applications/AsyncDelegateCommand.cs
@@ -1,53 +1,73 @@
-﻿using System.Windows.Input;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace System.Waf.Applications
 {
     /// <summary>
     /// Provides an <see cref="ICommand"/> implementation which relays the <see cref="Execute"/> and <see cref="CanExecute"/> 
     /// method to the specified delegates.
+    /// This implementation disables the command during the async command execution.
     /// </summary>
-    public class DelegateCommand : ICommand
+    public class AsyncDelegateCommand : ICommand
     {
-        private readonly Action<object> execute;
+        private readonly Func<object, Task> execute;
         private readonly Func<object, bool> canExecute;
-
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
-        /// </summary>
-        /// <param name="execute">Delegate to execute when Execute is called on the command.</param>
-        /// <exception cref="ArgumentNullException">The execute argument must not be null.</exception>
-        public DelegateCommand(Action execute) : this(execute, null) { }
+        private bool isExecuting;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand"/> class.
         /// </summary>
-        /// <param name="execute">Delegate to execute when Execute is called on the command.</param>
+        /// <param name="execute">Async Delegate to execute when Execute is called on the command.</param>
         /// <exception cref="ArgumentNullException">The execute argument must not be null.</exception>
-        public DelegateCommand(Action<object> execute) : this(execute, null) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
-        /// </summary>
-        /// <param name="execute">Delegate to execute when Execute is called on the command.</param>
-        /// <param name="canExecute">Delegate to execute when CanExecute is called on the command.</param>
-        /// <exception cref="ArgumentNullException">The execute argument must not be null.</exception>
-        public DelegateCommand(Action execute, Func<bool> canExecute)
-            : this(execute != null ? p => execute() : (Action<object>)null, canExecute != null ? p => canExecute() : (Func<object, bool>)null)
+        public AsyncDelegateCommand(Func<Task> execute)
+            : this(execute, null)
         { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand"/> class.
         /// </summary>
-        /// <param name="execute">Delegate to execute when Execute is called on the command.</param>
+        /// <param name="execute">Async Delegate to execute when Execute is called on the command.</param>
+        /// <exception cref="ArgumentNullException">The execute argument must not be null.</exception>
+        public AsyncDelegateCommand(Func<object, Task> execute)
+            : this(execute, null)
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand"/> class.
+        /// </summary>
+        /// <param name="execute">Async Delegate to execute when Execute is called on the command.</param>
         /// <param name="canExecute">Delegate to execute when CanExecute is called on the command.</param>
         /// <exception cref="ArgumentNullException">The execute argument must not be null.</exception>
-        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        public AsyncDelegateCommand(Func<Task> execute, Func<bool> canExecute)
+            : this(execute != null ? p => execute() : (Func<object, Task>)null, canExecute != null ? p => canExecute() : (Func<object, bool>)null)
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand"/> class.
+        /// </summary>
+        /// <param name="execute">Async Delegate to execute when Execute is called on the command.</param>
+        /// <param name="canExecute">Delegate to execute when CanExecute is called on the command.</param>
+        /// <exception cref="ArgumentNullException">The execute argument must not be null.</exception>
+        public AsyncDelegateCommand(Func<object, Task> execute, Func<object, bool> canExecute)
         {
             if (execute == null) { throw new ArgumentNullException(nameof(execute)); }
 
             this.execute = execute;
             this.canExecute = canExecute;
+        }
+
+
+        private bool IsExecuting
+        {
+            get { return isExecuting; }
+            set
+            {
+                if (isExecuting != value)
+                {
+                    isExecuting = value;
+                    RaiseCanExecuteChanged();
+                }
+            }
         }
 
 
@@ -64,22 +84,29 @@ namespace System.Waf.Applications
         /// <returns>true if this command can be executed; otherwise, false.</returns>
         public bool CanExecute(object parameter)
         {
-            return canExecute != null ? canExecute(parameter) : true;
+            return !IsExecuting && (canExecute == null || canExecute(parameter));
         }
 
         /// <summary>
         /// Defines the method to be called when the command is invoked.
         /// </summary>
         /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
-        /// <exception cref="InvalidOperationException">The <see cref="CanExecute"/> method returns <c>false.</c></exception>
-        public void Execute(object parameter)
+        public async void Execute(object parameter)
         {
             if (!CanExecute(parameter))
             {
-                throw new InvalidOperationException("The command cannot be executed because the canExecute action returned false.");
+                return;
             }
 
-            execute(parameter);
+            IsExecuting = true;
+            try
+            {
+                await execute(parameter);
+            }
+            finally
+            {
+                IsExecuting = false;
+            }
         }
 
         /// <summary>
