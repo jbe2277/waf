7614e5e823be50ac00cc566f3b90c4da695e6a06
jbe2277 <jbe2277@users.noreply.github.com>
2016-03-22 13:10:13 +0100

    Add StringHelper.Truncate

---
+++ src/System.Waf/System.Waf/System.Waf.Core.Test/Foundation/StringHelperTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Waf.Foundation;
+using System.Waf.UnitTesting;
+
+namespace Test.Waf.Foundation
+{
+    [TestClass]
+    public class StringHelperTest
+    {
+        [TestMethod]
+        public void TruncateTest()
+        {
+            Assert.AreSame("Hi", StringHelper.Truncate("Hi", 5));
+            Assert.AreSame("Hello", StringHelper.Truncate("Hello", 5));
+            Assert.AreEqual("Hello", StringHelper.Truncate("Hello World", 5));
+
+            Assert.AreEqual("", StringHelper.Truncate("Hi", 0));
+            Assert.IsNull(StringHelper.Truncate(null, 5));
+            Assert.AreEqual("", StringHelper.Truncate("", 5));
+            AssertHelper.ExpectedException<ArgumentOutOfRangeException>(() => StringHelper.Truncate("Hi", -5));
+        }
+    }
+}
---
+++ src/System.Waf/System.Waf/System.Waf.Core/Foundation/StringHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Waf.Foundation
+{
+    /// <summary>
+    /// Provides helper methods for working with strings.
+    /// </summary>
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Truncates the string to the specified maximum length.
+        /// </summary>
+        /// <param name="value">The string.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        /// <returns>The truncated string.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">maxLength is less than 0.</exception>
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) { return value; }
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}
