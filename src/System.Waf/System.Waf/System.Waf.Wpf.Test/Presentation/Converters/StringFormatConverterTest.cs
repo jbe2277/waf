b72f5c60cfd15c437b737b167d9c2e394592d65e
jbe2277 <jbe2277@users.noreply.github.com>
2016-04-28 22:08:44 +0200

    Enhance the StringFormatConverter to support MultiBinding

--- src/System.Waf/System.Waf/System.Waf.Wpf.Test/Presentation/Converters/StringFormatConverterTest.cs
+++ src/System.Waf/System.Waf/System.Waf.Wpf.Test/Presentation/Converters/StringFormatConverterTest.cs
@@ -18,7 +18,21 @@ namespace Test.Waf.Presentation.Converters
             Assert.AreEqual(string.Format(null, format, book), converter.Convert(book, null, format, null));
             Assert.AreEqual(string.Format(null, "{0}", book), converter.Convert(book, null, null, null));
 
-            AssertHelper.ExpectedException<NotSupportedException>(() => converter.ConvertBack(null, null, null, null));
+            AssertHelper.ExpectedException<NotSupportedException>(() => converter.ConvertBack(null, (Type)null, null, null));
+        }
+
+        [TestMethod]
+        public void StringFormatMultiConverterBasicTest()
+        {
+            string book = "Star Wars - Heir to the Empire";
+            string author = "Timothy Zahn";
+            string format = "Book: {0} by {1}";
+
+            StringFormatConverter converter = StringFormatConverter.Default;
+            Assert.AreEqual(string.Format(null, format, new[] { book, author }), converter.Convert(new[] { book, author }, null, format, null));
+            Assert.AreEqual(string.Format(null, "{0}", new[] { book, author }), converter.Convert(new[] { book, author }, null, null, null));
+
+            AssertHelper.ExpectedException<NotSupportedException>(() => converter.ConvertBack(null, (Type[])null, null, null));
         }
     }
 }
--- src/System.Waf/System.Waf/System.Waf.Wpf/Presentation/Converters/StringFormatConverter.cs
+++ src/System.Waf/System.Waf/System.Waf.Wpf/Presentation/Converters/StringFormatConverter.cs
@@ -12,6 +12,9 @@ namespace System.Waf.Presentation.Converters
     /// ConverterParameter property.
     /// </summary>
     public sealed class StringFormatConverter : IValueConverter
+#if !WINDOWS_UWP
+        , IMultiValueConverter
+#endif
     {
         /// <summary>
         /// Gets the default instance of this converter.
@@ -25,7 +28,7 @@ namespace System.Waf.Presentation.Converters
         /// <param name="value">The object to convert.</param>
         /// <param name="targetType">The type of the binding target property. This parameter will be ignored.</param>
         /// <param name="parameter">The format specification used to format the object.</param>
-        /// <param name="culture">The culture to use in the converter.</param>
+        /// <param name="culture">The culture to use in the converter. This parameter will be ignored.</param>
         /// <returns>The formatted string.</returns>
 #if WINDOWS_UWP
         public object Convert(object value, Type targetType, object parameter, string culture)
@@ -33,8 +36,7 @@ namespace System.Waf.Presentation.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 #endif
         {
-            string format = parameter as string ?? "{0}";
-            return string.Format(CultureInfo.CurrentCulture, format, value);
+            return string.Format(CultureInfo.CurrentCulture, parameter as string ?? "{0}", value);
         }
 
         /// <summary>
@@ -54,5 +56,34 @@ namespace System.Waf.Presentation.Converters
         {
             throw new NotSupportedException();
         }
+
+#if !WINDOWS_UWP
+        /// <summary>
+        /// Converts multiple objects into a formatted string.
+        /// </summary>
+        /// <param name="values">The objects to convert.</param>
+        /// <param name="targetType">The type of the binding target property. This parameter will be ignored.</param>
+        /// <param name="parameter">The format specification used to format the object.</param>
+        /// <param name="culture">The culture to use in the converter. This parameter will be ignored.</param>
+        /// <returns>The formatted string.</returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format(CultureInfo.CurrentCulture, parameter as string ?? "{0}", values);
+        }
+
+        /// <summary>
+        /// This method is not supported and throws an exception when it is called.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetTypes">The types to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>Nothing because this method throws an exception.</returns>
+        /// <exception cref="NotSupportedException">Throws this exception when the method is called.</exception>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+#endif
     }
 }
