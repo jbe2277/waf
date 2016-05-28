bb6e264d787491528a08c50549defa26583cb337
jbe2277 <jbe2277@users.noreply.github.com>
2016-05-28 17:27:57 +0200

    Add unit test project for NewsReader.Domain

---
+++ src/NewsReader/NewsReader.Domain.Test/FeedItemTest.cs
@@ -0,0 +1,45 @@
+﻿using Jbe.NewsReader.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Waf.UnitTesting;
+
+namespace Test.NewsReader.Domain
+{
+    [TestClass]
+    public class FeedItemTest
+    {
+        [TestMethod]
+        public void ApplyValuesFromTest()
+        {
+            AssertHelper.ExpectedException<ArgumentNullException>(() => new FeedItem(null, DateTimeOffset.Now, "test", "test", "test"));
+
+            var itemA1 = new FeedItem(new Uri("http://www.test.com/rss/feed"), new DateTimeOffset(2020, 5, 5, 12, 0, 0, new TimeSpan(1, 0, 0)), "name", "desc", "author");
+            Assert.AreEqual(new DateTimeOffset(2020, 5, 5, 12, 0, 0, new TimeSpan(1, 0, 0)), itemA1.Date);
+            Assert.AreEqual("name", itemA1.Name);
+            Assert.AreEqual("desc", itemA1.Description);
+            Assert.AreEqual("author", itemA1.Author);
+
+            var itemA2 = new FeedItem(new Uri("http://www.test.com/rss/feed"), new DateTimeOffset(2022, 5, 5, 12, 0, 0, new TimeSpan(1, 0, 0)), "name2", "desc2", "author2");
+            itemA1.ApplyValuesFrom(itemA2);
+            Assert.AreEqual(new DateTimeOffset(2022, 5, 5, 12, 0, 0, new TimeSpan(1, 0, 0)), itemA1.Date);
+            Assert.AreEqual("name2", itemA1.Name);
+            Assert.AreEqual("desc2", itemA1.Description);
+            Assert.AreEqual("author2", itemA1.Author);
+
+            var itemB1 = new FeedItem(new Uri("http://www.test.com/rss/feed2"), new DateTimeOffset(2020, 5, 5, 12, 0, 0, new TimeSpan(1, 0, 0)), "name", "desc", "author");
+            AssertHelper.ExpectedException<InvalidOperationException>(() => itemA1.ApplyValuesFrom(itemB1));
+        }
+
+        [TestMethod]
+        public void SupportNull()
+        {
+            var item = new FeedItem(new Uri("http://www.test.com/rss/feed"), new DateTimeOffset(2020, 5, 5, 12, 0, 0, new TimeSpan(1, 0, 0)), "name", "desc", "author");
+            item.Name = null;
+            item.Description = null;
+            item.Author = null;
+            Assert.IsNull(item.Name);
+            Assert.IsNull(item.Description);
+            Assert.IsNull(item.Author);
+        }
+    }
+}
