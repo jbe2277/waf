759da57e22512394ae81eda0b9d54cf254a87a6a
jbe2277 <jbe2277@users.noreply.github.com>
2016-06-11 23:46:13 +0200

    NR Use the WebView with the SeparateThread mode for improved responsiveness

--- src/NewsReader/NewsReader.Presentation/Views/FeedItemView.xaml.cs
+++ src/NewsReader/NewsReader.Presentation/Views/FeedItemView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Jbe.NewsReader.Applications.ViewModels;
 using Jbe.NewsReader.Applications.Views;
+using Jbe.NewsReader.Domain;
 using System;
+using System.ComponentModel;
 using System.Composition;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
@@ -12,12 +14,16 @@ namespace Jbe.NewsReader.Presentation.Views
     public sealed partial class FeedItemView : UserControl, IFeedItemView
     {
         private readonly Lazy<FeedItemViewModel> viewModel;
+        private readonly WebView webView;
         private TaskCompletionSource<object> unloadedTaskSource;
-
+        private FeedItem feedItem;
+        
 
         public FeedItemView()
         {
             InitializeComponent();
+            webView = new WebView(WebViewExecutionMode.SeparateThread) { Visibility = Visibility.Collapsed };
+            webViewPresenter.Content = webView;
             viewModel = new Lazy<FeedItemViewModel>(() => (FeedItemViewModel)DataContext);
             unloadedTaskSource = new TaskCompletionSource<object>();
             unloadedTaskSource.SetResult(null);
@@ -32,18 +38,68 @@ namespace Jbe.NewsReader.Presentation.Views
 
         private bool IsLoaded => !unloadedTaskSource.Task.IsCompleted;
 
+        private FeedItem FeedItem
+        {
+            get { return feedItem; }
+            set
+            {
+                if (feedItem != value)
+                {
+                    if (feedItem != null)
+                    {
+                        feedItem.PropertyChanged -= FeedItemPropertyChanged;
+                    }
+                    feedItem = value;
+                    UpdateWebView();
+                    if (feedItem != null)
+                    {
+                        feedItem.PropertyChanged += FeedItemPropertyChanged;
+                    }
+                }
+            }
+        }
+        
 
         private async void LoadedHandler(object sender, RoutedEventArgs e)
         {
             // Workaround because the Windows Runtime does not guarantee that first the Unloaded handler is called before the second Loaded handler comes.
             await unloadedTaskSource.Task;
             unloadedTaskSource = new TaskCompletionSource<object>();
-            webView.Navigate(webView.Source);
+            ViewModel.SelectionService.PropertyChanged += SelectionServicePropertyChanged;
+            FeedItem = ViewModel.SelectionService.SelectedFeedItem;
         }
 
         private void UnloadedHandler(object sender, RoutedEventArgs e)
         {
+            ViewModel.SelectionService.PropertyChanged -= SelectionServicePropertyChanged;
+            FeedItem = null;
             unloadedTaskSource.SetResult(null);
+        }
+
+        private void SelectionServicePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(ViewModel.SelectionService.SelectedFeedItem))
+            {
+                FeedItem = ViewModel.SelectionService.SelectedFeedItem;
+            }
+        }
+
+        private void FeedItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(FeedItem.Uri))
+            {
+                UpdateWebView();
+            }
+        }
+
+        private void UpdateWebView()
+        {
+            webView.Visibility = FeedItem != null ? Visibility.Visible : Visibility.Collapsed;
+            var uri = FeedItem?.Uri;
+            if (uri != null && webView.Source != uri)
+            {
+                webView.Navigate(uri);
+            }
         }
 
         private void WebViewNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs e)
