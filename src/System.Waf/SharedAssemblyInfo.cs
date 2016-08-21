ff767a07879d411374d28060ee11827eeaf28c0c
jbe2277 <jbe2277@users.noreply.github.com>
2016-08-21 22:08:48 +0200

    WAF Change version number to 4.0.0.6

--- src/System.Waf/SharedAssemblyInfo.cs
+++ src/System.Waf/SharedAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US")]
 
 
-[assembly: AssemblyVersion("4.0.0.5")]
-[assembly: AssemblyFileVersion("4.0.0.5")]
-[assembly: AssemblyInformationalVersion("4.0.0-alpha5")]
+[assembly: AssemblyVersion("4.0.0.6")]
+[assembly: AssemblyFileVersion("4.0.0.6")]
+[assembly: AssemblyInformationalVersion("4.0.0-alpha6")]
