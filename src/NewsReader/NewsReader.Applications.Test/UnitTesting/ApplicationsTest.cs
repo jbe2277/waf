c7b51cbe138406dadcb577d0ff277395675d84e2
jbe2277 <jbe2277@users.noreply.github.com>
2016-09-05 22:15:00 +0200

    Add simple ShellViewModel unit test

--- src/NewsReader/NewsReader.Applications.Test/UnitTesting/ApplicationsTest.cs
+++ src/NewsReader/NewsReader.Applications.Test/UnitTesting/ApplicationsTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Jbe.NewsReader.Applications.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Composition.Hosting;
+using System.Reflection;
 using Test.NewsReader.Domain.UnitTesting;
 
 namespace Test.NewsReader.Applications.UnitTesting
@@ -6,6 +9,17 @@ namespace Test.NewsReader.Applications.UnitTesting
     [TestClass]
     public abstract class ApplicationsTest : DomainTest
     {
-        
+        protected CompositionHost Container { get; private set; }
+
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            var configuration = new ContainerConfiguration()
+                .WithAssembly(typeof(ShellViewModel).GetTypeInfo().Assembly)
+                .WithAssembly(typeof(ApplicationsTest).GetTypeInfo().Assembly);
+            Container = configuration.CreateContainer();
+        }
     }
 }
--- src/NewsReader/NewsReader.Applications/Properties/AssemblyInfo.cs
+++ src/NewsReader/NewsReader.Applications/Properties/AssemblyInfo.cs
@@ -1,5 +1,9 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("NewsReader.Applications")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+
+
+[assembly: InternalsVisibleTo("Test.NewsReader.Applications")]
