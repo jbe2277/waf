52c67a748319d839ead065cdf5a2a7e0f837645a
jbe2277 <jbe2277@users.noreply.github.com>
2017-02-04 22:06:22 +0100

    NR Update version number to 2.0.7.0

--- src/NewsReader/SharedAssemblyInfo.cs
+++ src/NewsReader/SharedAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US")]
 
 
-[assembly: AssemblyVersion("2.0.6.0")]
-[assembly: AssemblyFileVersion("2.0.6.0")]
+[assembly: AssemblyVersion("2.0.7.0")]
+[assembly: AssemblyFileVersion("2.0.7.0")]
