af93f0ac620edab6f11fab656f45fcf09f6444b3
jbe2277 <jbe2277@users.noreply.github.com>
2017-04-02 14:16:18 +0200

    NR Hide list item navigation buttons when SelectionMode == None

---
+++ src/NewsReader/NewsReader.Presentation/Converters/SelectionModeToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace Jbe.NewsReader.Presentation.Converters
+{
+    public class SelectionModeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var mode = value as ListViewSelectionMode?;
+            return mode == ListViewSelectionMode.None ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
