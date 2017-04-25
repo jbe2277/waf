ab67e13ad98c6b96a5b1c0b1ac6658bf5c64f864
jbe2277 <jbe2277@users.noreply.github.com>
2017-04-25 22:00:18 +0200

    NR Update version number to 3.1.1.0

--- src/NewsReader/SharedAssemblyInfo.cs
+++ src/NewsReader/SharedAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US")]
 
 
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
