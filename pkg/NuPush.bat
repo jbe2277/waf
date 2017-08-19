c1c333f753219d4bf1902f0dba7e79334f75f51f
jbe2277 <jbe2277@users.noreply.github.com>
2017-08-19 14:08:24 +0200

    Update NuGet publish and Changelog

--- pkg/NuPush.bat
+++ pkg/NuPush.bat
@@ -4,10 +4,10 @@ set PkgDir=%~dp0
 set PATH=%PATH%;%PkgDir%
 set Version=4.1.0-alpha6
 
-cd %PkgDir%\System.Waf\Release
+cd %PkgDir%\System.Waf\Release\%Version%
 
-nuget Push System.Waf.Core.%Version%.nupkg
-nuget Push System.Waf.UnitTesting.Core.%Version%.nupkg
-nuget Push System.Waf.Wpf.%Version%.nupkg
-nuget Push System.Waf.UnitTesting.Wpf.%Version%.nupkg
-nuget Push System.Waf.Uwp.%Version%.nupkg
+nuget Push System.Waf.Core.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
+nuget Push System.Waf.UnitTesting.Core.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
+nuget Push System.Waf.Wpf.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
+nuget Push System.Waf.UnitTesting.Wpf.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
+nuget Push System.Waf.Uwp.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
