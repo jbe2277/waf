b5ec642c2d891ae4676d898ee5b7488c7c8bfa00
jbe2277 <jbe2277@users.noreply.github.com>
2017-08-27 22:44:13 +0200

    NR Update version number to 4.0.3.0

--- src/NewsReader/SharedAssemblyInfo.cs
+++ src/NewsReader/SharedAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US")]
 
 
-[assembly: AssemblyVersion("4.0.2.0")]
-[assembly: AssemblyFileVersion("4.0.2.0")]
+[assembly: AssemblyVersion("4.0.3.0")]
+[assembly: AssemblyFileVersion("4.0.3.0")]
