31bbb6bbdba5b072c0145189faea6daa80301eee
jbe2277 <jbe2277@users.noreply.github.com>
2017-08-27 17:24:40 +0200

    WAF NuPush version 4.1.0.91

--- pkg/NuPush.bat
+++ pkg/NuPush.bat
@@ -2,12 +2,13 @@
 setlocal
 set PkgDir=%~dp0
 set PATH=%PATH%;%PkgDir%
-set Version=4.1.0-alpha7
+set Version=4.1.0-rc1
 
-cd %PkgDir%\System.Waf\Release\%Version%
+cd %PkgDir%\System.Waf\Release
 
 nuget Push System.Waf.Core.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
 nuget Push System.Waf.UnitTesting.Core.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
 nuget Push System.Waf.Wpf.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
 nuget Push System.Waf.UnitTesting.Wpf.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
-nuget Push System.Waf.Uwp.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
+
+nuget Push System.Waf.Uwp.4.1.0-alpha8.nupkg -Source https://www.nuget.org/api/v2/package
