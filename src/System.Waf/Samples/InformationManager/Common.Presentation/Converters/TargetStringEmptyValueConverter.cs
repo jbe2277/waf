0394d43fddbd7a236c0fdf384dc9580651b5c08d
jbe2277 <jbe2277@users.noreply.github.com>
2018-02-10 21:55:51 +0100

    InfoMan: Replace TargetNullValue with the TargetStringEmptyValueConverter

---
+++ src/System.Waf/Samples/InformationManager/Common.Presentation/Converters/TargetStringEmptyValueConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Waf.InformationManager.Common.Presentation.Converters
+{
+    /// <summary>
+    /// Value converter that returns a special string if the (string) value is null or empty.
+    /// </summary>
+    public class TargetStringEmptyValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Returns a special string if the (string) value is null or empty. Otherwise it returns value.
+        /// </summary>
+        /// <param name="value">The string value to convert.</param>
+        /// <param name="targetType">The type of the binding target property. This parameter will be ignored.</param>
+        /// <param name="parameter">This parameter will be ignored.</param>
+        /// <param name="culture">The culture to use in the converter. This parameter will be ignored.</param>
+        /// <returns>The converted string.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty(value as string) ? "(none)" : value;
+        }
+
+        /// <summary>
+        /// This method is not supported and throws an exception when it is called.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>Nothing because this method throws an exception.</returns>
+        /// <exception cref="NotSupportedException">Throws this exception when the method is called.</exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
