936beb0817bebe99cf4f6463e2ba566410b0cc1e
jbe2277 <jbe2277@users.noreply.github.com>
2018-07-18 23:13:59 +0200

    WAF Improve Merge unit tests

--- src/System.Waf/System.Waf/System.Waf.Core.Test/Foundation/CollectionHelperTest.cs
+++ src/System.Waf/System.Waf/System.Waf.Core.Test/Foundation/CollectionHelperTest.cs
@@ -100,47 +100,49 @@ namespace Test.Waf.Foundation
         [TestMethod]
         public void MergeTest()
         {
-            MergeTestCore(new[] { "1", "2" }, new[] { "1", "2" }, ExpectedCollectionChange.None);
+            MergeTestCore(new[] { "1", "2" }, new[] { "1", "2" }.ToList(), ExpectedCollectionChange.None);
 
-            MergeTestCore(new[] { "1", "2", "3" }, new[] { "1", "2" }, ExpectedCollectionChange.Insert);
-            MergeTestCore(new[] { "1", "2", "3" }, new[] { "1", "3" }, ExpectedCollectionChange.Insert);
-            MergeTestCore(new[] { "1", "2", "3" }, new[] { "2", "3" }, ExpectedCollectionChange.Insert);
+            MergeTestCore(new[] { "1", "2", "3" }, new[] { "1", "2" }.ToList(), ExpectedCollectionChange.Insert);
+            MergeTestCore(new[] { "1", "2", "3" }, new[] { "1", "3" }.ToList(), ExpectedCollectionChange.Insert);
+            MergeTestCore(new[] { "1", "2", "3" }, new[] { "2", "3" }.ToList(), ExpectedCollectionChange.Insert);
 
-            MergeTestCore(new[] { "1", "2" }, new[] { "1", "2", "3" }, ExpectedCollectionChange.Remove);
-            MergeTestCore(new[] { "1", "3" }, new[] { "1", "2", "3" }, ExpectedCollectionChange.Remove);
-            MergeTestCore(new[] { "2", "3" }, new[] { "1", "2", "3" }, ExpectedCollectionChange.Remove);
+            MergeTestCore(new[] { "1", "2" }, new[] { "1", "2", "3" }.ToList(), ExpectedCollectionChange.Remove);
+            MergeTestCore(new[] { "1", "3" }, new[] { "1", "2", "3" }.ToList(), ExpectedCollectionChange.Remove);
+            MergeTestCore(new[] { "2", "3" }, new[] { "1", "2", "3" }.ToList(), ExpectedCollectionChange.Remove);
 
-            MergeTestCore(new[] { "1", "2" }, new[] { "2", "3" }, ExpectedCollectionChange.Reset);
-            MergeTestCore(new[] { "2", "4", "5" }, new[] { "2", "3" }, ExpectedCollectionChange.Reset);
-            MergeTestCore(new[] { "4" }, new[] { "2", "3" }, ExpectedCollectionChange.Reset);
+            MergeTestCore(new[] { "1", "2" }, new[] { "2", "3" }.ToList(), ExpectedCollectionChange.Reset);
+            MergeTestCore(new[] { "2", "4", "5" }, new[] { "2", "3" }.ToList(), ExpectedCollectionChange.Reset);
+            MergeTestCore(new[] { "4" }, new[] { "2", "3" }.ToList(), ExpectedCollectionChange.Reset);
 
-            MergeTestCore(new[] { "1", "2" }, new[] { "2", "1" }, ExpectedCollectionChange.None, null, 1);
-            MergeTestCore(new[] { "1", "2", "3" }, new[] { "3", "1", "2" }, ExpectedCollectionChange.None, null, 2);
-            MergeTestCore(new[] { "1", "2" }, new[] { "1" }, ExpectedCollectionChange.Insert, null, 0);
-            MergeTestCore(new[] { "1", "2" }, new[] { "1", "3" }, ExpectedCollectionChange.Reset, null, 0);
+            MergeTestCore(new[] { "1", "2" }, new[] { "2", "1" }.ToList(), ExpectedCollectionChange.None, null, 1);
+            MergeTestCore(new[] { "1", "2", "3" }, new[] { "3", "1", "2" }.ToList(), ExpectedCollectionChange.None, null, 1);
+            MergeTestCore(new[] { "1", "2", "3", "4" }, new[] { "4", "3", "2", "1" }.ToList(), ExpectedCollectionChange.None, null, 3);
+            MergeTestCore(new[] { "1", "2" }, new[] { "1" }.ToList(), ExpectedCollectionChange.Insert, null, 0);
+            MergeTestCore(new[] { "1", "2" }, new[] { "1", "3" }.ToList(), ExpectedCollectionChange.Reset, null, 0);
         }
 
         [TestMethod]
         public void MergeTestWithComparer()
         {
-            MergeTestCore(new[] { "a", "b" }, new[] { "A", "B" }, ExpectedCollectionChange.None, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "a", "b" }, new[] { "A", "B" }.ToList(), ExpectedCollectionChange.None, StringComparer.OrdinalIgnoreCase);
 
-            MergeTestCore(new[] { "a", "b", "c" }, new[] { "A", "B" }, ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase);
-            MergeTestCore(new[] { "a", "b", "c" }, new[] { "A", "C" }, ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase);
-            MergeTestCore(new[] { "a", "b", "c" }, new[] { "B", "C" }, ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "a", "b", "c" }, new[] { "A", "B" }.ToList(), ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "a", "b", "c" }, new[] { "A", "C" }.ToList(), ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "a", "b", "c" }, new[] { "B", "C" }.ToList(), ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase);
 
-            MergeTestCore(new[] { "a", "b" }, new[] { "A", "B", "C" }, ExpectedCollectionChange.Remove, StringComparer.OrdinalIgnoreCase);
-            MergeTestCore(new[] { "a", "c" }, new[] { "A", "B", "C" }, ExpectedCollectionChange.Remove, StringComparer.OrdinalIgnoreCase);
-            MergeTestCore(new[] { "b", "c" }, new[] { "A", "B", "C" }, ExpectedCollectionChange.Remove, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "a", "b" }, new[] { "A", "B", "C" }.ToList(), ExpectedCollectionChange.Remove, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "a", "c" }, new[] { "A", "B", "C" }.ToList(), ExpectedCollectionChange.Remove, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "b", "c" }, new[] { "A", "B", "C" }.ToList(), ExpectedCollectionChange.Remove, StringComparer.OrdinalIgnoreCase);
 
-            MergeTestCore(new[] { "a", "b" }, new[] { "B", "C" }, ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase);
-            MergeTestCore(new[] { "b", "d", "e" }, new[] { "B", "C" }, ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase);
-            MergeTestCore(new[] { "d" }, new[] { "B", "C" }, ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "a", "b" }, new[] { "B", "C" }.ToList(), ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "b", "d", "e" }, new[] { "B", "C" }.ToList(), ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase);
+            MergeTestCore(new[] { "d" }, new[] { "B", "C" }.ToList(), ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase);
 
-            MergeTestCore(new[] { "a", "b" }, new[] { "B", "A" }, ExpectedCollectionChange.None, StringComparer.OrdinalIgnoreCase, 1);
-            MergeTestCore(new[] { "a", "b", "c" }, new[] { "C", "A", "B" }, ExpectedCollectionChange.None, StringComparer.OrdinalIgnoreCase, 2);
-            MergeTestCore(new[] { "a", "b" }, new[] { "A" }, ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase, 0);
-            MergeTestCore(new[] { "a", "b" }, new[] { "A", "C" }, ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase, 0);
+            MergeTestCore(new[] { "a", "b" }, new[] { "B", "A" }.ToList(), ExpectedCollectionChange.None, StringComparer.OrdinalIgnoreCase, 1);
+            MergeTestCore(new[] { "a", "b", "c" }, new[] { "C", "A", "B" }.ToList(), ExpectedCollectionChange.None, StringComparer.OrdinalIgnoreCase, 1);
+            MergeTestCore(new[] { "a", "b", "c", "d" }, new[] { "D", "C", "B", "A" }.ToList(), ExpectedCollectionChange.None, StringComparer.OrdinalIgnoreCase, 3);
+            MergeTestCore(new[] { "a", "b" }, new[] { "A" }.ToList(), ExpectedCollectionChange.Insert, StringComparer.OrdinalIgnoreCase, 0);
+            MergeTestCore(new[] { "a", "b" }, new[] { "A", "C" }.ToList(), ExpectedCollectionChange.Reset, StringComparer.OrdinalIgnoreCase, 0);
         }
 
         private void MergeTestCore<T>(IList<T> source, IList<T> target, ExpectedCollectionChange expectedCollectionChange, IEqualityComparer<T> comparer = null, int expectedMoveCounts = -1)
@@ -149,19 +151,33 @@ namespace Test.Waf.Foundation
             var removeCounter = 0;
             var resetCounter = 0;
             var moveCounter = 0;
-            Action<int, T> insertAction = (idx, item) => insertCounter++;
-            Action<int> removeAction = idx => removeCounter++;
-            Action resetAction = () => resetCounter++;
+            Action<int, T> insertAction = (idx, item) => 
+            {
+                target.Insert(idx, item);
+                insertCounter++;
+            };
+            Action<int> removeAction = idx =>
+            {
+                target.RemoveAt(idx);
+                removeCounter++;
+            };
+            Action resetAction = () =>
+            {
+                target.Clear();
+                foreach (var item in source) target.Add(item);
+                resetCounter++;
+            };
             Action<int, int> moveAction = null;
             if (expectedMoveCounts >= 0) moveAction = (int oldIndex, int newIndex) =>
             {
-                T swap = target[oldIndex];
-                target[oldIndex] = target[newIndex];
-                target[newIndex] = swap;
+                T item = target[oldIndex];
+                target.RemoveAt(oldIndex);
+                target.Insert(newIndex, item);
                 moveCounter++;
             };
 
             target.Merge(new ReadOnlyCollection<T>(source), comparer, insertAction, removeAction, resetAction, moveAction);
+            Assert.IsTrue(target.SequenceEqual(source, comparer ?? EqualityComparer<T>.Default));
             Assert.AreEqual(expectedCollectionChange == ExpectedCollectionChange.Insert ? 1 : 0, insertCounter);
             Assert.AreEqual(expectedCollectionChange == ExpectedCollectionChange.Remove ? 1 : 0, removeCounter);
             Assert.AreEqual(expectedCollectionChange == ExpectedCollectionChange.Reset ? 1 : 0, resetCounter);
