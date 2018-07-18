a5a1f72d3e5a419be2c019dd729ae98af819f2d5
jbe2277 <jbe2277@users.noreply.github.com>
2018-07-18 21:36:45 +0200

    WAF Add the ObservableListView

---
+++ src/System.Waf/System.Waf/System.Waf.Core/Foundation/ObservableListView.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace System.Waf.Foundation
+{
+    /// <summary>
+    /// Represents a observable list view for filtering and sorting a data collection.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    public class ObservableListView<T> : ObservableListViewBase<T>, IDisposable
+    {
+        private readonly IEnumerable<T> originalList;
+        private readonly IEqualityComparer<T> comparer;
+        private readonly INotifyCollectionChanged originalObservableCollection;
+        private Predicate<T> filter;
+        private Func<IEnumerable<T>, IOrderedEnumerable<T>> sort;
+        private volatile bool isDisposed;
+
+
+        /// <summary>
+        /// Initializes a new instance of the ObservableListView class that represents a view of the specified list.
+        /// </summary>
+        /// <param name="originalList">The orignal list.</param>
+        public ObservableListView(IEnumerable<T> originalList) : this(originalList, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ObservableListView class that represents a view of the specified list.
+        /// </summary>
+        /// <param name="originalList">The orignal list.</param>
+        /// <param name="comparer">The IEqualityComparer implementation to use when comparing items.</param>
+        public ObservableListView(IEnumerable<T> originalList, IEqualityComparer<T> comparer) : base(originalList)
+        {
+            this.originalList = originalList;
+            this.comparer = comparer ?? EqualityComparer<T>.Default;
+
+            originalObservableCollection = originalList as INotifyCollectionChanged;
+            if (originalObservableCollection != null)
+            {
+                originalObservableCollection.CollectionChanged += OriginalCollectionChanged;
+            }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the filter used for this list view.
+        /// </summary>
+        public Predicate<T> Filter
+        {
+            get { return filter; }
+            set
+            {
+                if (filter != value)
+                {
+                    filter = value;
+                    UpdateInnerList();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the sorting used for this list view.
+        /// </summary>
+        public Func<IEnumerable<T>, IOrderedEnumerable<T>> Sort
+        {
+            get { return sort; }
+            set
+            {
+                if (sort != value)
+                {
+                    sort = value;
+                    UpdateInnerList();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Updates the list view and raises the appropriate collection changed events.
+        /// If the original list implements the INotifyCollectionChanged interface then calling this method is not necessary.
+        /// </summary>
+        public void Update()
+        {
+            UpdateInnerList();
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Call this method from a subclass when you implement a finalizer (destructor).
+        /// </summary>
+        /// <param name="disposing">if true then dispose unmanaged and managed resources; otherwise dispose only unmanaged resources.</param>
+        protected void Dispose(bool disposing)
+        {
+            if (isDisposed) { return; }
+            isDisposed = true;
+
+            OnDispose(disposing);
+            if (disposing)
+            {
+                if (originalObservableCollection != null)
+                {
+                    originalObservableCollection.CollectionChanged -= OriginalCollectionChanged;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Override this method to free, release or reset any resources.
+        /// </summary>
+        /// <param name="disposing">if true then dispose unmanaged and managed resources; otherwise dispose only unmanaged resources.</param>
+        protected virtual void OnDispose(bool disposing) { }
+
+        private void UpdateInnerList()
+        {
+            var enumerable = originalList;
+            if (Filter != null)
+            {
+                enumerable = enumerable.Where(x => Filter(x));
+            }
+            if (Sort != null)
+            {
+                enumerable = Sort(enumerable);
+            }
+            var newList = enumerable.ToArray();
+            InnerList.Merge(newList.ToArray(), comparer, Insert, RemoveAt, () => Reset(newList), Move);
+        }
+
+        private void OriginalCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateInnerList();
+        }
+    }
+}
---
+++ src/System.Waf/System.Waf/System.Waf.Core/Foundation/ObservableListViewBase.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace System.Waf.Foundation
+{
+    /// <summary>
+    /// Provides the base class for a generic observable read-only collection.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    public abstract class ObservableListViewBase<T> : ReadOnlyCollection<T>, INotifyPropertyChanged, IReadOnlyObservableList<T>
+    {
+        private const string indexerName = "Item[]";  // This must be equal to Binding.IndexerName
+        private static readonly PropertyChangedEventArgs CountChangedEventArgs = new PropertyChangedEventArgs(nameof(Count));
+        private static readonly PropertyChangedEventArgs IndexerChangedEventArgs = new PropertyChangedEventArgs(indexerName);
+
+
+        /// <summary>
+        /// Initializes a new instance of the ObservableListViewBase class.
+        /// </summary>
+        /// <param name="originalList">Initialize the list view with the items from this list.</param>
+        protected ObservableListViewBase(IEnumerable<T> originalList) : base(new List<T>())
+        {
+            InnerList = (List<T>)Items;
+            if (originalList != null) InnerList.AddRange(originalList);
+        }
+
+
+        /// <summary>
+        /// The inner list of this list view.
+        /// </summary>
+        protected List<T> InnerList { get; }
+
+
+        /// <summary>
+        /// Occurs when an item is added, removed, changed, moved, or the entire list is refreshed.
+        /// </summary>
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// Raises the CollectionChanged event with the provided arguments.
+        /// </summary>
+        /// <param name="e">Arguments of the event being raised.</param>
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanged?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanged event with the provided arguments.
+        /// </summary>
+        /// <param name="e">Arguments of the event being raised.</param>
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Inserts an element into the list at the specified index.
+        /// </summary>
+        /// <param name="newItemIndex">The zero-based index at which item should be inserted.</param>
+        /// <param name="newItem">The object to insert.</param>
+        protected void Insert(int newItemIndex, T newItem)
+        {
+            InnerList.Insert(newItemIndex, newItem);
+            OnPropertyChanged(CountChangedEventArgs);
+            OnPropertyChanged(IndexerChangedEventArgs);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newItem, newItemIndex));
+        }
+
+        /// <summary>
+        /// Removes the element at the specified index.
+        /// </summary>
+        /// <param name="oldItemIndex">The zero-based index of the element to remove.</param>
+        protected void RemoveAt(int oldItemIndex)
+        {
+            var oldItem = InnerList[oldItemIndex];
+            InnerList.RemoveAt(oldItemIndex);
+            OnPropertyChanged(CountChangedEventArgs);
+            OnPropertyChanged(IndexerChangedEventArgs);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItem, oldItemIndex));
+        }
+
+        /// <summary>
+        /// Replace all items with the items of the new list.
+        /// </summary>
+        /// <param name="newList">The items of the new list.</param>
+        protected void Reset(IEnumerable<T> newList)
+        {
+            InnerList.Clear();
+            InnerList.AddRange(newList);
+            OnPropertyChanged(CountChangedEventArgs);
+            OnPropertyChanged(IndexerChangedEventArgs);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        /// <summary>
+        /// Moves the item at the specified index to a new location in the collection.
+        /// </summary>
+        /// <param name="oldIndex">The zero-based index specifying the location of the item to be moved.</param>
+        /// <param name="newIndex">The zero-based index specifying the new location of the item.</param>
+        protected void Move(int oldIndex, int newIndex)
+        {
+            T item = InnerList[oldIndex];
+            InnerList.RemoveAt(oldIndex);
+            InnerList.Insert(newIndex, item);
+            OnPropertyChanged(IndexerChangedEventArgs);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, item, newIndex, oldIndex));
+        }
+    }
+}
