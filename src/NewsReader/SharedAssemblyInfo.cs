0db6215cbf7d4282e68b0b7266585bf9c16f1ec8
jbe2277 <jbe2277@users.noreply.github.com>
2018-11-30 14:55:18 +0100

    NR Update version number to 5.1.0.0

--- src/NewsReader/SharedAssemblyInfo.cs
+++ src/NewsReader/SharedAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US")]
 
 
-[assembly: AssemblyVersion("5.0.5.0")]
-[assembly: AssemblyFileVersion("5.0.5.0")]
+[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyFileVersion("5.1.0.0")]
