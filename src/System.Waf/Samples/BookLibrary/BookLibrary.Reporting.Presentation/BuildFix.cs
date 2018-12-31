6c07661c658e0aadd7b9f8016657fe0f9e44525c
jbe2277 <jbe2277@users.noreply.github.com>
2018-12-31 23:08:37 +0100

    BookLib: Fix build

---
+++ src/System.Waf/Samples/BookLibrary/BookLibrary.Reporting.Presentation/BuildFix.cs
@@ -0,0 +1,10 @@
+﻿namespace Waf.BookLibrary.Reporting.Presentation
+{
+    internal class BuildFix
+    {
+        public static void JustToCopyReferenceIntoOutput()
+        {
+            typeof(Applications.Views.IReportView).GetHashCode();
+        }
+    }
+}
