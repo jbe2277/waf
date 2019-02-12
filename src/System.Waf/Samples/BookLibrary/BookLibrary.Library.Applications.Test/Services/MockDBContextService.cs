13209028425ad8887ea29600a13c037d9c0b7060
jbe2277 <jbe2277@users.noreply.github.com>
2019-02-12 21:47:24 +0100

    BookLib: Fix InMemory context for unit tests

--- src/System.Waf/Samples/BookLibrary/BookLibrary.Library.Applications.Test/Services/MockDBContextService.cs
+++ src/System.Waf/Samples/BookLibrary/BookLibrary.Library.Applications.Test/Services/MockDBContextService.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Composition;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Waf.BookLibrary.Library.Applications.Data;
 using Waf.BookLibrary.Library.Applications.Services;
 using Waf.BookLibrary.Library.Domain;
@@ -12,7 +13,8 @@ namespace Test.BookLibrary.Library.Applications.Services
         public DbContext GetBookLibraryContext(out string dataSourcePath)
         {
             dataSourcePath = @"C:\Test.db";
-            var options = new DbContextOptionsBuilder<BookLibraryContext>().UseInMemoryDatabase(databaseName: "TestDatabase").Options;
+            var options = new DbContextOptionsBuilder<BookLibraryContext>().UseInMemoryDatabase(databaseName: "TestDatabase", 
+                databaseRoot: new InMemoryDatabaseRoot()).Options;
             var context = new BookLibraryContext(options, modelBuilder =>
             {
                 modelBuilder.Entity<Book>().Ignore(x => x.Errors).Ignore(x => x.HasErrors);
