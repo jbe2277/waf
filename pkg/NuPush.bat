d4e95de418ec1abeb1bc786666f7532e9d5520c8
jbe2277 <jbe2277@users.noreply.github.com>
2019-02-17 21:30:45 +0100

    WAF Update NuPush.bat

--- pkg/NuPush.bat
+++ pkg/NuPush.bat
@@ -2,13 +2,13 @@
 setlocal
 set PkgDir=%~dp0
 set PATH=%PATH%;%PkgDir%
-set Version=4.3.0
+set Version=5.0.0-alpha2
 
 cd %PkgDir%\System.Waf\Release\%Version%
 
-nuget Push System.Waf.Core.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
-nuget Push System.Waf.UnitTesting.Core.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
-nuget Push System.Waf.Wpf.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
-nuget Push System.Waf.UnitTesting.Wpf.%Version%.nupkg -Source https://www.nuget.org/api/v2/package
+nuget Push System.Waf.Core.%Version%.nupkg -Source https://api.nuget.org/v3/index.json
+nuget Push System.Waf.UnitTesting.Core.%Version%.nupkg -Source https://api.nuget.org/v3/index.json
+nuget Push System.Waf.Wpf.%Version%.nupkg -Source https://api.nuget.org/v3/index.json
+nuget Push System.Waf.UnitTesting.Wpf.%Version%.nupkg -Source https://api.nuget.org/v3/index.json
 
-rem nuget Push System.Waf.Uwp.4.1.0-alpha8.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget Push System.Waf.Uwp.4.1.0-alpha8.nupkg -Source https://api.nuget.org/v3/index.json
