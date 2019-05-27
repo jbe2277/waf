de6b4ff93533bbc9b12ae8eebb99c7104a4a88a1
jbe2277 <jbe2277@users.noreply.github.com>
2019-05-27 20:45:07 +0200

    WAF Move ApplicationInfo class from WPF to Core assembly

---
+++ src/System.Waf/System.Waf/System.Waf.Core.Test/Applications/ApplicationInfoTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Waf.Applications;
+
+namespace Test.Waf.Applications
+{
+    [TestClass]
+    public class ApplicationInfoTest
+    {
+        [TestMethod]
+        public void ApplicationInfoPropertiesTest()
+        {
+            // This doesn't work because the Visual Studio Unit Testing Framework
+            // doesn't have an entry assembly
+            
+            string productName = ApplicationInfo.ProductName;
+            string version = ApplicationInfo.Version;
+            string company = ApplicationInfo.Company;
+            string copyright = ApplicationInfo.Copyright;
+            Assert.IsNotNull(ApplicationInfo.ApplicationPath);
+
+            // The second time it returns the cached values
+
+            productName = ApplicationInfo.ProductName;
+            version = ApplicationInfo.Version;
+            company = ApplicationInfo.Company;
+            copyright = ApplicationInfo.Copyright;
+            Assert.IsNotNull(ApplicationInfo.ApplicationPath);
+        }
+    }
+}
---
+++ src/System.Waf/System.Waf/System.Waf.Core/Applications/ApplicationInfo.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace System.Waf.Applications
+{
+    /// <summary>
+    /// This class provides information about the running application.
+    /// </summary>
+    public static class ApplicationInfo
+    {
+        private static readonly Lazy<string> productName = new Lazy<string>(GetProductName);
+        private static readonly Lazy<string> version = new Lazy<string>(GetVersion);
+        private static readonly Lazy<string> company = new Lazy<string>(GetCompany);
+        private static readonly Lazy<string> copyright = new Lazy<string>(GetCopyright);
+        private static readonly Lazy<string> applicationPath = new Lazy<string>(GetApplicationPath);
+
+        /// <summary>
+        /// Gets the product name of the application.
+        /// </summary>
+        public static string ProductName => productName.Value;
+
+        /// <summary>
+        /// Gets the version number of the application.
+        /// </summary>
+        public static string Version => version.Value;
+
+        /// <summary>
+        /// Gets the company of the application.
+        /// </summary>
+        public static string Company => company.Value;
+        
+        /// <summary>
+        /// Gets the copyright information of the application.
+        /// </summary>
+        public static string Copyright => copyright.Value;
+
+        /// <summary>
+        /// Gets the path for the executable file that started the application, not including the executable name.
+        /// </summary>
+        public static string ApplicationPath => applicationPath.Value;
+
+
+        private static string GetProductName()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            if (entryAssembly != null)
+            {
+                var attribute = (AssemblyProductAttribute)Attribute.GetCustomAttribute(entryAssembly, typeof(AssemblyProductAttribute));
+                return attribute?.Product ?? "";
+            }
+            return "";
+        }
+
+        private static string GetVersion()
+        {
+            return Assembly.GetEntryAssembly()?.GetName().Version.ToString() ?? "";
+        }
+
+        private static string GetCompany()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            if (entryAssembly != null)
+            {
+                var attribute = (AssemblyCompanyAttribute)Attribute.GetCustomAttribute(entryAssembly, typeof(AssemblyCompanyAttribute));
+                return attribute?.Company ?? "";
+            }
+            return "";
+        }
+
+        private static string GetCopyright()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            if (entryAssembly != null)
+            {
+                var attribute = (AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(entryAssembly, typeof(AssemblyCopyrightAttribute));
+                return attribute?.Copyright ?? "";
+            }
+            return "";
+        }
+
+        private static string GetApplicationPath()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            if (entryAssembly != null)
+            {
+                return Path.GetDirectoryName(entryAssembly.Location);
+            }
+            return "";
+        }
+    }
+}
