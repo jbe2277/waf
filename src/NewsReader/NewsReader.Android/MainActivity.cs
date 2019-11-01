00cfe32386c0207e13c0cfad22cbca9aa9ec650c
jbe2277 <jbe2277@users.noreply.github.com>
2019-11-01 22:11:31 +0100

    NR Android Fix authentication issue with MS Graph

--- src/NewsReader/NewsReader.Android/MainActivity.cs
+++ src/NewsReader/NewsReader.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Xamarin.Forms;
 using Waf.NewsReader.Applications;
 using Waf.NewsReader.Presentation;
 using Autofac;
+using Android.Content;
+using Microsoft.Identity.Client;
 
 namespace Waf.NewsReader.Android
 {
@@ -33,10 +35,17 @@ namespace Waf.NewsReader.Android
             var container = builder.Build();
             LoadApplication(container.Resolve<App>());
         }
+        
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
         }
     }
 }
