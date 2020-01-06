bd16e1fccd5557392e07b0bafdad8030f65194fb
jbe2277 <jbe2277@users.noreply.github.com>
2020-01-06 22:05:01 +0100

    NR Update NuGet packages

--- src/NewsReader/NewsReader.Presentation/GlobalSuppressions.cs
+++ src/NewsReader/NewsReader.Presentation/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1001:Types that own disposable fields should be disposable", Justification = "<Pending>", Scope = "type", Target = "~T:Waf.NewsReader.Presentation.Views.FeedItemView")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA5350:Do Not Use Weak Cryptographic Algorithms", Justification = "<Pending>", Scope = "member", Target = "~M:Waf.NewsReader.Presentation.Services.DataService.GetHash~System.String")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:Waf.NewsReader.Presentation.Services.WebStorageService.TrySilentSignIn~System.Threading.Tasks.Task{System.Boolean}")]
