6163e8929218f980a82191c36ca12899ec3422af
jbe2277 <jbe2277@users.noreply.github.com>
2023-01-05 22:34:56 +0100

    WAF: IView: add default implementation for BindingContext (requires .NET 6)

---
+++ src/System.Waf/System.Waf/System.Waf.Core.Test/Applications/ViewTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Waf.Applications;
+
+namespace Test.Waf.Applications
+{
+    [TestClass]
+    public class ViewTest
+    {
+        [TestMethod]
+        public void ViewImplementationTest()
+        {
+            var v1 = new MyView1();
+            var vm1 = new MyViewModel(v1);
+            Assert.AreSame(vm1, v1.DataContext);
+#if NET6_0_OR_GREATER
+            Assert.IsNull(((IView)v1).BindingContext);
+
+            var v2 = new MyView2();
+            var vm2 = new MyViewModel(v2);
+            Assert.AreSame(vm2, ((IView)v2).DataContext);
+            Assert.AreSame(vm2, v2.BindingContext);
+
+            var v3 = new MyView3();
+            var vm3 = new MyViewModel(v3);
+            Assert.IsNull(((IView)v3).DataContext);
+            Assert.IsNull(((IView)v3).BindingContext);
+#endif
+        }
+
+        private class MyViewModel : ViewModelCore<IView>
+        {
+            public MyViewModel(IView view) : base(view) { }
+        }
+
+        private sealed class MyView1 : IView
+        {
+            public object? DataContext { get; set; }
+        }
+
+#if NET6_0_OR_GREATER
+        private sealed class MyView2 : IView
+        {
+            public object? BindingContext { get; set; }
+        }
+
+        private sealed class MyView3 : IView { }
+#endif
+    }
+}
--- src/System.Waf/System.Waf/System.Waf.Core/Applications/IView.cs
+++ src/System.Waf/System.Waf/System.Waf.Core/Applications/IView.cs
@@ -1,9 +1,20 @@
 ﻿namespace System.Waf.Applications
 {
-    /// <summary>Represents a view</summary>
+#if NET6_0_OR_GREATER
+    /// <summary>Represents a view. Implement either DataContext or BindingContext but not both.</summary>
+#else
+    /// <summary>Represents a view.</summary>
+#endif
     public interface IView
     {
         /// <summary>Gets or sets the data context of the view.</summary>
+#if NET6_0_OR_GREATER
+        object? DataContext { get => BindingContext; set => BindingContext = value; }
+
+        /// <summary>Gets or sets the binding context of the view.</summary>
+        object? BindingContext  { get => null; set { } }
+#else
         object? DataContext { get; set; }
+#endif
     }
 }
