7776319f18b2a254f625a296b61724d913aa3a19
jbe2277 <jbe2277@users.noreply.github.com>
2023-08-22 22:18:48 +0200

    NR: suppress warnings

--- src/NewsReader/NewsReader.MauiSystem/GlobalSuppressions.cs
+++ src/NewsReader/NewsReader.MauiSystem/GlobalSuppressions.cs
@@ -1,1 +1,13 @@
-﻿[assembly: SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Waf.NewsReader.MauiSystem.Platforms.iOS.AppDelegate")]
+﻿
+
+#if IOS
+[assembly: SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Waf.NewsReader.MauiSystem.Platforms.iOS.AppDelegate")]
+
+#elif ANDROID
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "<Pending>", Scope = "type", Target = "~T:Waf.NewsReader.MauiSystem.Platforms.Android.Services.AndroidSystemTraceListener")]
+
+#elif WINDOWS
+[assembly: SuppressMessage("Security", "CA5392:Use DefaultDllImportSearchPaths attribute for P/Invokes", Justification = "<Pending>", Scope = "member", Target = "~M:Waf.NewsReader.MauiSystem.Platform.Windows.Program.XamlCheckProcessRequirements")]
+[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "<Pending>", Scope = "type", Target = "~T:Waf.NewsReader.MauiSystem.Platforms.Windows.Services.WindowsSystemTraceListener")]
+
+#endif
--- src/NewsReader/NewsReader.Presentation/GlobalSuppressions.cs
+++ src/NewsReader/NewsReader.Presentation/GlobalSuppressions.cs
@@ -1,3 +1,4 @@
 ﻿[assembly: SuppressMessage("Design", "CA1001:Types that own disposable fields should be disposable", Justification = "<Pending>", Scope = "type", Target = "~T:Waf.NewsReader.Presentation.Views.FeedItemView")]
 [assembly: SuppressMessage("Security", "CA5350:Do Not Use Weak Cryptographic Algorithms", Justification = "<Pending>", Scope = "member", Target = "~M:Waf.NewsReader.Presentation.Services.DataService.GetHash~System.String")]
 [assembly: SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes", Scope = "type", Target = "~T:Waf.NewsReader.Presentation.Services.WebStorageService")]
+[assembly: SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "<Pending>", Scope = "type", Target = "~T:Waf.NewsReader.Presentation.App")]
