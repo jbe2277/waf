fdf67d59eb87d6e92535db2c516a3b0a0b9d12ba
jbe2277 <jbe2277@users.noreply.github.com>
2023-10-04 22:12:13 +0200

    WAF: MockSettingsService move to UnitTesting.Core

---
+++ src/System.Waf/System.Waf/System.Waf.UnitTesting.Core/Mocks/MockSettingsService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+using System.Waf.Applications.Services;
+
+namespace System.Waf.UnitTesting.Mocks
+{
+    /// <summary>Mock for the ISettingsService interface.</summary>
+    public class MockSettingsService : ISettingsService
+    {
+        private readonly ConcurrentDictionary<Type, Lazy<object>> services = new();
+
+        /// <summary>Gets or sets a delegate that is called when Get is called.</summary>
+        public Func<Type, object>? GetStub { get; set; }
+
+        /// <summary>Gets or sets a delegate that is called when Save is called.</summary>
+        public Action? SaveStub { get; set; }
+
+        /// <inheritdoc />
+        public string FileName { get; set; } = default!;
+
+        /// <inheritdoc />
+        public event EventHandler<SettingsErrorEventArgs>? ErrorOccurred;
+
+        /// <inheritdoc />
+        public T Get<T>() where T : class, new() => (T)(GetStub?.Invoke(typeof(T)) ?? services.GetOrAdd(typeof(T), new Lazy<object>(() => new T())).Value);
+
+        /// <inheritdoc />
+        public void Save() => SaveStub?.Invoke();
+
+        /// <summary>Raise the ErrorOccurred event.</summary>
+        /// <param name="e">The error event args.</param>
+        public void RaiseErrorOccurred(SettingsErrorEventArgs e) => ErrorOccurred?.Invoke(this, e);
+    }
+}
---
+++ src/System.Waf/System.Waf/System.Waf.UnitTesting.Wpf/Mocks/MefMockSettingsService.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.Composition;
+using System.Waf.Applications.Services;
+
+namespace System.Waf.UnitTesting.Mocks
+{
+    /// <summary>Mock for the ISettingsService interface.</summary>
+    [Export(typeof(ISettingsService)), Export(typeof(MockSettingsService)), Export]
+    public class MefMockSettingsService : MockSettingsService
+    {
+    }
+}
