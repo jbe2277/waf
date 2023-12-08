866e5253726c233765512ac2e21a1b86ba19998f
jbe2277 <jbe2277@users.noreply.github.com>
2023-12-08 15:06:16 +0100

    WAF add test for MockView

---
+++ src/System.Waf/System.Waf/System.Waf.Core.Test/UnitTesting/Mocks/MockViewTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Waf.Applications;
+using System.Waf.UnitTesting.Mocks;
+
+namespace Test.Waf.UnitTesting.Mocks
+{
+    [TestClass]
+    public class MockViewTest
+    {
+        [TestMethod]
+        public void MockViewContextTest()
+        {
+            IView v1 = new MyView();
+            var vm1 = new MyViewModel(v1);
+            Assert.AreSame(vm1, v1.DataContext);
+#if NET6_0_OR_GREATER
+            Assert.AreSame(vm1, v1.BindingContext);
+#endif
+        }
+
+        private class MyViewModel : ViewModelCore<IView>
+        {
+            public MyViewModel(IView view) : base(view) { }
+        }
+
+        private sealed class MyView : MockView
+        {
+        }
+    }
+}
