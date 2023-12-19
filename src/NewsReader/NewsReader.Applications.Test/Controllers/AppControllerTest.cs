610e8c30e98a0ecd2d7239c089311b04dcc61a17
jbe2277 <jbe2277@users.noreply.github.com>
2023-12-19 21:29:49 +0100

    NR test: improve OpenSettingsView test

--- src/NewsReader/NewsReader.Applications.Test/Controllers/AppControllerTest.cs
+++ src/NewsReader/NewsReader.Applications.Test/Controllers/AppControllerTest.cs
@@ -11,10 +11,14 @@ public class AppControllerTest : TestBase
     {
         StartApp();
         var feed = Context.WaitForNotNull(Shell.View.GetCurrentViewOrNull<MockFeedView, FeedViewModel>);
-        Assert.Equal(FeedsController.FeedManager.Feeds.Single(), feed.ViewModel.Feed);
+        var feed1 = FeedsController.FeedManager.Feeds.Single();
+        Assert.Equal(feed1, feed.ViewModel.Feed);
+        Assert.Equal(feed1, Shell.ViewModel.SelectedFeed);
+        Assert.Null(Shell.ViewModel.SelectedFooterMenu);
 
         Shell.ViewModel.SelectedFooterMenu = Shell.ViewModel.FooterMenu[1];
         var settings = Shell.View.GetCurrentView<MockSettingsView, SettingsViewModel>();
         Assert.NotNull(settings.View);
+        Assert.Null(Shell.ViewModel.SelectedFeed);
     }
 }
--- src/NewsReader/NewsReader.Applications.Test/Services/MockDataService.cs
+++ src/NewsReader/NewsReader.Applications.Test/Services/MockDataService.cs
@@ -8,7 +8,7 @@ public class MockDataService : IDataService
 
     public Stream GetReadStream() => throw new NotImplementedException();
 
-    public T? Load<T>(Stream? dataStream = null) where T : class => throw new NotImplementedException();
+    public T? Load<T>(Stream? dataStream = null) where T : class => null;
 
     public void Save(object data) => throw new NotImplementedException();
 }
