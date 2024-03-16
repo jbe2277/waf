1525508aa3fe9a74062c52511ddfe8b315b9573a
jbe2277 <jbe2277@users.noreply.github.com>
2024-03-16 22:16:29 +0100

    Writer: add another integration test

--- src/System.Waf/Samples/Writer/Writer.Applications.Test/Views/MockStartView.cs
+++ src/System.Waf/Samples/Writer/Writer.Applications.Test/Views/MockStartView.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.Composition;
 using System.Waf.UnitTesting.Mocks;
+using Waf.Writer.Applications.ViewModels;
 using Waf.Writer.Applications.Views;
 
 namespace Test.Writer.Applications.Views;
 
 [Export(typeof(IStartView))]
-public class MockStartView : MockView, IStartView
+public class MockStartView : MockView<StartViewModel>, IStartView
 {
 }
--- src/System.Waf/Samples/Writer/Writer.Presentation.Test/IntegrationTest.cs
+++ src/System.Waf/Samples/Writer/Writer.Presentation.Test/IntegrationTest.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Waf.UnitTesting.Mocks;
 using Test.Writer.Applications.Services;
+using Test.Writer.Applications.Views;
+using Waf.Writer.Applications.ViewModels;
+using Waf.Writer.Applications.Views;
 
 namespace Test.Writer.Presentation;
 
@@ -22,5 +25,40 @@ public class IntegrationTest : PresentationTest
 
         Assert.AreEqual(MessageType.Error, messageService.MessageType);
         Assert.IsFalse(string.IsNullOrEmpty(messageService.Message));
+    }
+
+    [TestMethod]
+    public void NewModifyAndCloseWithoutSave()
+    {
+        StartApp();
+        var shellViewModel = Get<ShellViewModel>();
+        var mainView = (MockMainView)shellViewModel.ContentView;
+        Assert.AreEqual(ContentViewState.StartViewVisible, mainView.ContentViewState);
+        var mainViewModel = mainView.ViewModel;
+        
+        mainViewModel.FileService.NewCommand.Execute(null);
+        Assert.AreEqual(ContentViewState.DocumentViewVisible, mainView.ContentViewState);
+        var richTextView = (MockRichTextView)mainViewModel.ActiveDocumentView!;
+        var richTextViewModel = richTextView.ViewModel;
+        var shellService = shellViewModel.ShellService;
+        
+        Assert.IsFalse(shellService.ActiveZoomCommands.ZoomInCommand.CanExecute(null));
+        richTextViewModel.IsVisible = true;
+        Assert.IsTrue(shellService.ActiveZoomCommands.ZoomInCommand.CanExecute(null));
+        Assert.AreSame(richTextViewModel, shellService.ActiveEditingCommands);
+        
+        richTextViewModel.Document.Modified = true;
+
+        bool showDialogActionShown = false;
+        MockSaveChangesView.ShowDialogAction = v =>
+        {
+            showDialogActionShown = true;
+            v.ViewModel.NoCommand.Execute(null);
+        };
+
+        mainViewModel.FileService.CloseCommand.Execute(null);
+        Assert.IsTrue(showDialogActionShown);
+        Assert.IsNull(mainViewModel.ActiveDocumentView);
+        Assert.AreEqual(ContentViewState.StartViewVisible, mainView.ContentViewState);
     }
 }
