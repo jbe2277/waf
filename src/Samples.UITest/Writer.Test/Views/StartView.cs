2e4afef49bf8718b8364ba17451bf5f856148dd4
jbe2277 <jbe2277@users.noreply.github.com>
2024-04-21 15:44:45 +0200

    UITest: Extend test NewSaveRestartOpenChangeAskToSave

--- src/Samples.UITest/Writer.Test/Views/StartView.cs
+++ src/Samples.UITest/Writer.Test/Views/StartView.cs
@@ -6,4 +6,15 @@ namespace UITest.Writer.Views;
 public class StartView(FrameworkAutomationElementBase element) : AutomationElement(element)
 {
     public Button NewButton => this.Find("NewButton").AsButton();
+
+    public ListBox RecentFilesList => this.Find("RecentFilesList").AsListBox();
+
+    public RecentFilesListItem[] RecentFilesListItems => RecentFilesList.Items.Select(x => x.As<RecentFilesListItem>()).ToArray();
+}
+
+public class RecentFilesListItem(FrameworkAutomationElementBase element) : ListBoxItem(element)
+{
+    public string FileName => OpenFile.Name;
+
+    public Button OpenFile => this.Find("RecentItemOpenLink").AsButton();
 }
