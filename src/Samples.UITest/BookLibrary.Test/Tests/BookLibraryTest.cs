72a003d0059e049a3b10655a34ab2ed85822413b
jbe2277 <jbe2277@users.noreply.github.com>
2024-05-05 21:32:37 +0200

    UITest.BookLibrary: add About test

--- src/Samples.UITest/BookLibrary.Test/Tests/BookLibraryTest.cs
+++ src/Samples.UITest/BookLibrary.Test/Tests/BookLibraryTest.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Capturing;
+using UITest.SystemViews;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace UITest.BookLibrary.Tests;
@@ -10,6 +13,20 @@ public class BookLibraryTest(ITestOutputHelper log) : UITest(log)
     {
         Launch();
         var window = GetShellWindow();
-        Thread.Sleep(3000);
+
+        var helpMenu = window.HelpMenu;
+        helpMenu.Click();
+        helpMenu.AboutMenuItem.Click();
+
+        var messageBox = window.FirstModalWindow().As<MessageBox>();
+        Assert.Equal("Waf Book Library", messageBox.Title);
+        Log.WriteLine(messageBox.Message);
+        Assert.StartsWith("Waf Book Library ", messageBox.Message);
+        Capture.Screen().ToFile(GetScreenshotFile("About.png"));
+        messageBox.OkButton.Click();
+
+        var dataMenu = window.DataMenu;
+        dataMenu.Click();
+        dataMenu.ExitMenuItem.Click();
     }
 }
