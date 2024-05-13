84ed84d7b8d540b28758f96628c7a1c6d67ae8cc
jbe2277 <jbe2277@users.noreply.github.com>
2024-05-13 21:27:07 +0200

    BookLib: Add CLI argument to start app with defined culture settings; add automation ids

--- src/System.Waf/Samples/BookLibrary/BookLibrary.Library.Presentation/Properties/Settings.Designer.cs
+++ src/System.Waf/Samples/BookLibrary/BookLibrary.Library.Presentation/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Waf.BookLibrary.Library.Presentation.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace Waf.BookLibrary.Library.Presentation.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
-<ArrayOfString xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+<ArrayOfString xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
   <string>Waf.BookLibrary.Reporting.Applications.dll</string>
   <string>Waf.BookLibrary.Reporting.Presentation.dll</string>
 </ArrayOfString>")]
