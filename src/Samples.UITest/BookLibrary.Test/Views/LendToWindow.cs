bebfc020bfc14d02b4bde403e954591f9e7bf2aa
jbe2277 <jbe2277@users.noreply.github.com>
2024-05-16 22:01:20 +0200

    UITest: BookLib: SearchBookListAndChangeEntriesTest

---
+++ src/Samples.UITest/BookLibrary.Test/Views/LendToWindow.cs
@@ -0,0 +1,17 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+
+namespace UITest.BookLibrary.Views;
+
+public class LendToWindow(FrameworkAutomationElementBase element) : Window(element)
+{
+    public RadioButton WasReturnedRadioButton => this.Find("WasReturnedRadioButton").AsRadioButton();
+
+    public RadioButton LendToRadioButton => this.Find("LendToRadioButton").AsRadioButton();
+
+    public ListBox PersonListBox => this.Find("PersonListBox").AsListBox();
+
+    public Button OkButton => this.Find("OkButton").AsButton();
+
+    public Button CancelButton => this.Find("CancelButton").AsButton();
+}
