4356ca5ca8ec9d8a5b3c382ced5b3cf73b5ed096
jbe2277 <jbe2277@users.noreply.github.com>
2024-06-08 11:48:58 +0200

    Add CreateReportTest

---
+++ src/Samples.UITest/BookLibrary.Test/Tests/ReportingTest.cs
@@ -0,0 +1,30 @@
+﻿using FlaUI.Core.Capturing;
+using FlaUI.Core.Definitions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UITest.BookLibrary.Tests;
+
+public class ReportingTest(ITestOutputHelper log) : UITest(log)
+{
+    [Fact]
+    public void CreateReportTest() => Run(() =>
+    {
+        Launch();
+        var window = GetShellWindow();
+        window.SetState(WindowVisualState.Maximized);
+        window.TabControl.ReportingTabItem.Select();
+        var reportView = window.TabControl.ReportingTabItem.ReportView;
+
+        Assert.False(reportView.PrintButton.IsEnabled);  // No report available
+        
+        reportView.CreateBookListReportButton.Click();
+        Assert.True(reportView.PrintButton.IsEnabled);
+        Capture.Screen().ToFile(GetScreenshotFile("BookListReport"));
+
+        reportView.CreateBorrowedBooksReportButton.Click();
+        Capture.Screen().ToFile(GetScreenshotFile("BorrowedBooksReport"));
+
+        window.Close();
+    });
+}
