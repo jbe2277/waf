64812cb8fdabe0d1dda6047c7a701e05a989b559
jbe2277 <jbe2277@users.noreply.github.com>
2024-06-16 13:50:04 +0200

    Log list of available printers

--- src/Samples.UITest/UITest.Core/SystemViews/LegacyPrintDialog.cs
+++ src/Samples.UITest/UITest.Core/SystemViews/LegacyPrintDialog.cs
@@ -6,6 +6,8 @@ namespace UITest.SystemViews;
 
 public class LegacyPrintDialog(FrameworkAutomationElementBase element) : Window(element)
 {
+    public ListBox PrinterList => this.Find(x => x.ByControlType(ControlType.List).And(x.ByAutomationId("1"))).AsListBox();
+
     public Button PrintButton => this.Find(x => x.ByControlType(ControlType.Button).And(x.ByAutomationId("1"))).AsButton();
 
     public Button CancelButton => this.Find(x => x.ByControlType(ControlType.Button).And(x.ByAutomationId("2"))).AsButton();
