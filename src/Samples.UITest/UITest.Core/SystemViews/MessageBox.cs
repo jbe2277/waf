58bb2284f842aec44f35c5c34083ce25128f9660
jbe2277 <jbe2277@users.noreply.github.com>
2024-07-03 22:18:35 +0200

    UITest: MessageBox remove property which is already inherited

--- src/Samples.UITest/UITest.Core/SystemViews/MessageBox.cs
+++ src/Samples.UITest/UITest.Core/SystemViews/MessageBox.cs
@@ -6,8 +6,6 @@ namespace UITest.SystemViews;
 
 public class MessageBox(FrameworkAutomationElementBase element) : Window(element)
 {
-    public string Title => Name;
-
     public string Message => this.Find(x => x.ByControlType(ControlType.Text)).Name;
 
     public Button[] Buttons => this.FindAll(x => x.ByControlType(ControlType.Button)).Select(x => x.AsButton()).ToArray();
