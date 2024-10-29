6b42c602704b10cecaac23f5b7e411d058c9bd51
jbe2277 <jbe2277@users.noreply.github.com>
2024-10-29 17:39:22 +0100

    Build: add code coverage analysis to BuildRelease.bat

--- build/BuildRelease.bat
+++ build/BuildRelease.bat
@@ -1,2 +1,11 @@
 setlocal
-dotnet build %~dp0..\src\System.Waf\System.Waf.sln -c Release -p:ContinuousIntegrationBuild=true
+set DOTNET_CLI_UI_LANGUAGE=en
+cd %~dp0../src
+
+dotnet build System.Waf/System.Waf.sln -c Release -p:ContinuousIntegrationBuild=true
+
+dotnet test System.Waf/System.Waf.sln -c Release --no-build --collect "Code Coverage"
+
+dotnet-coverage merge --output ../out/CodeCoverageReport/System.Waf.cobertura.xml --output-format cobertura "./**/TestResults/**/*.coverage"
+
+reportgenerator -reports:../out/CodeCoverageReport/System.Waf.cobertura.xml -targetdir:../out/CodeCoverageReport -reporttypes:"MarkdownSummaryGithub"
