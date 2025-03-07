1c6f7c2ed1f3f919a23cea7cd588675bd8c69309
jbe2277 <jbe2277@users.noreply.github.com>
2025-03-07 20:27:35 +0100

    Build: Update NuGetPush.bat

--- build/NuGetPush.bat
+++ build/NuGetPush.bat
@@ -1,8 +1,8 @@
 setlocal
 set Params=-s https://api.nuget.org/v3/index.json --force-english-output
-set Version=7.0.0
+set Version=8.1.0
 
-cd %~dp0..\src\System.Waf\System.Waf\
+cd %HOMEDRIVE%%HOMEPATH%\Downloads\Packages\
 
 dotnet nuget push .\System.Waf.Core\bin\Release\System.Waf.Core.%Version%.nupkg %Params%
 dotnet nuget push .\System.Waf.UnitTesting.Core\bin\Release\System.Waf.UnitTesting.Core.%Version%.nupkg %Params%
