7217cdcb7c54ed03086aebddfc26b742d0c2c84c
jbe2277 <jbe2277@users.noreply.github.com>
2025-05-03 11:30:21 +0200

    NR: Improve styling of navigation buttons

--- src/NewsReader/NewsReader.Applications/ViewModels/ShellViewModel.cs
+++ src/NewsReader/NewsReader.Applications/ViewModels/ShellViewModel.cs
@@ -36,7 +36,7 @@ public class ShellViewModel(IShellView view, IAppInfoService appInfoService) : V
             if (!SetProperty(ref selectedFooterMenu, value)) return;
             if (selectedFooterMenu is not null)
             {
-                SelectedFeed = null;  // TODO: CollectionView shows this still as selected! Also initial Feed selection not shown (Windows)
+                SelectedFeed = null;
                 if (!suppressSelectedFooterMenuCommand) selectedFooterMenu.Command?.Execute(null);
             }
         }
