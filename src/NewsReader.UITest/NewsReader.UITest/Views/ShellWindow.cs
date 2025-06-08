72d5927724b76342658b527c9354830272bb2142
jbe2277 <jbe2277@users.noreply.github.com>
2025-06-08 10:44:32 +0200

    NR/UITest: fix MenuButton accessor

--- src/NewsReader.UITest/NewsReader.UITest/Views/ShellWindow.cs
+++ src/NewsReader.UITest/NewsReader.UITest/Views/ShellWindow.cs
@@ -4,7 +4,7 @@ namespace UITest.NewsReader.Views;
 
 public record ShellWindow(AppiumDriver Driver)
 {
-    public AppiumElement MenuButton = Driver.Find(MobileBy.AccessibilityId("Open navigation drawer"));
+    public AppiumElement MenuButton => Driver.Find(MobileBy.AccessibilityId("Open navigation drawer"));
     
     public AppiumElement SettingsItem => Driver.Find("SettingsItem");
 
