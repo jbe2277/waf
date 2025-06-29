f9b015eef3e4515b4710b9984dacdd51cf14c764
jbe2277 <jbe2277@users.noreply.github.com>
2025-06-29 22:11:26 +0200

    NR/UITest: Add OpenFirstNewsTest (#95)

--- src/NewsReader.UITest/NewsReader.UITest/NewsReaderTest.cs
+++ src/NewsReader.UITest/NewsReader.UITest/NewsReaderTest.cs
@@ -8,12 +8,36 @@ namespace UITest.NewsReader;
 public abstract class NewsReaderTest : UITest
 {
     [Fact]
+    public void OpenFirstNewsTest()
+    {
+        if (IsWindows) Driver.Manage().Window.Maximize();
+        var window = GetShellWindow();
+        if (IsAndroid) window.MenuButton.SafeClick();
+        var menuView = window.MenuView;
+        var firstItem = menuView.FeedNavigationItems[0];        
+        Log.WriteLine(("1. Feed:", firstItem.TitleLabel.Text));
+        firstItem.Element.SafeClick();
+        if (IsAndroid) window.TapEmptySpace();  // Close menu flyout
+
+        var feedView = window.FeedView;
+        Log.WriteLine("Feed items:");
+        foreach (var x in feedView.FeedItems) Log.WriteLine("  " + x.NameLabel.Text);
+        feedView.FeedItems[0].Element.SafeClick();
+
+        var feedItemView = window.FeedItemView;
+        Thread.Sleep(1000);
+        CreateScreenshot("FeedView");        
+
+        window.Back();
+    }
+
+    [Fact]
     public void InfoViewTest()
     {
         if (IsWindows) Driver.Manage().Window.Maximize();
         var window = GetShellWindow();
         if (IsAndroid) window.MenuButton.SafeClick();
-        window.SettingsItem.SafeClick();
+        window.MenuView.SettingsItem.SafeClick();
         var settingsView = window.SettingsView;
         settingsView.InfoTabButton.SafeClick();
         Log.WriteLine(("Version:", settingsView.InfoView.VersionLabel.Text));
