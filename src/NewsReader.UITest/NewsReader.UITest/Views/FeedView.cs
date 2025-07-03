35555e0462e7dfbf91fc97b4bab412eb65b147c0
jbe2277 <jbe2277@users.noreply.github.com>
2025-07-03 16:59:38 +0200

    NR/UITest: Extend test to cover SearchBar

--- src/NewsReader.UITest/NewsReader.UITest/Views/FeedView.cs
+++ src/NewsReader.UITest/NewsReader.UITest/Views/FeedView.cs
@@ -1,9 +1,17 @@
 ﻿using OpenQA.Selenium.Appium;
+using UITest.NewsReader.Controls;
 
 namespace UITest.NewsReader.Views;
 
 public record FeedView(AppiumElement Element)
 {
+    public AppiumElement SearchButton => Element.OnPlatform(
+        android: () => Element.GetDriver().Find(MobileBy.AccessibilityId("SearchButton")),
+        windows: () => Element.GetDriver().Find("SearchButton"),
+        iOS: () => throw new NotSupportedException());
+
+    public SearchBar SearchBar => new(Element.Find("SearchBar"));
+
     public IReadOnlyList<FeedItem> FeedItems => Element.Find("FeedItemList").FindAll("FeedItem").Select(x => new FeedItem(x)).ToArray();
 }
 
