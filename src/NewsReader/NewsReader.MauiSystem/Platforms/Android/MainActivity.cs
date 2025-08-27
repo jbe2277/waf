d2815d0855ca34baa64c0297f4a6641bbe1d6cb8
jbe2277 <jbe2277@users.noreply.github.com>
2025-08-27 19:46:17 +0200

    NR: MAUI and Android - use Light mode; disable support for dark mode

--- src/NewsReader/NewsReader.MauiSystem/Platforms/Android/MainActivity.cs
+++ src/NewsReader/NewsReader.MauiSystem/Platforms/Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.OS;
+using AndroidX.AppCompat.App;
 using Microsoft.Identity.Client;
 
 namespace Waf.NewsReader.MauiSystem.Platforms.Android;
@@ -8,6 +10,12 @@ namespace Waf.NewsReader.MauiSystem.Platforms.Android;
 [Activity(Name = "Waf.NewsReader.MainActivity", Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightNo;
+    }
+
     protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
     {
         base.OnActivityResult(requestCode, resultCode, data);
