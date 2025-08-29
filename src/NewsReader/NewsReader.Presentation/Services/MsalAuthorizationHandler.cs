2515020d9504f788541e0680cc9e5a5c2ccb6f4b
jbe2277 <jbe2277@users.noreply.github.com>
2025-08-29 22:22:25 +0200

    NR: Remove MS Graph SDK and use plain HttpClient instead - reduce app size and is more efficient

---
+++ src/NewsReader/NewsReader.Presentation/Services/MsalAuthorizationHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Identity.Client;
+using System.Net.Http.Headers;
+using System.Security.Authentication;
+
+namespace Waf.NewsReader.Presentation.Services;
+
+internal sealed class MsalAuthorizationHandler : DelegatingHandler
+{
+    private readonly IPublicClientApplication client;
+    private readonly string[] scopes;
+
+    public MsalAuthorizationHandler(IPublicClientApplication client, string[] scopes)
+    {
+        this.client = client;
+        this.scopes = scopes;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var accounts = await client.GetAccountsAsync().ConfigureAwait(false);
+        if (!accounts.Any()) throw new AuthenticationException("No account found");
+        var result = await client.AcquireTokenSilent(scopes, accounts.First()).ExecuteAsync(cancellationToken).ConfigureAwait(false);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+        return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+    }
+}
