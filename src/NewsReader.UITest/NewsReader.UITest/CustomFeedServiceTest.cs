dfbbd836f41abc90b1ecda16d3fcccb9065d0677
jbe2277 <jbe2277@users.noreply.github.com>
2025-09-12 19:47:52 +0200

    NR/UITest: Add first draft of CustomFeedServiceTest

---
+++ src/NewsReader.UITest/NewsReader.UITest/CustomFeedServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using UITest.FeedService;
+using Xunit;
+
+namespace UITest.NewsReader;
+
+[DeviceCollectionTrait(DevicePlatform.Windows)] public class WindowsCustomFeedServiceTest : CustomFeedServiceTest { }
+
+public abstract class CustomFeedServiceTest : UITest
+{
+    [Fact]
+    public async Task CustomFeedServiceAddAndListTest()
+    {
+        var cts = new CancellationTokenSource();
+        var serviceTask = FeedWebApp.RunService(new SyndicationData(), null, cts.Token);
+
+        if (IsWindows) Driver.Manage().Window.Maximize();
+        var window = GetShellWindow();
+        if (!IsWindows) window.MenuButton.SafeClick();
+        var menuView = window.MenuView;
+        menuView.AddFeedItem.SafeClick();
+        var addEditFeedView = window.AddEditFeedView;
+        addEditFeedView.FeedUrlEntry.EnterText("http://localhost:5000/feed/rss");
+        addEditFeedView.LoadFeedButton.SafeClick();
+
+        Assert.Equal("FeedTitle", addEditFeedView.FeedNameEntry.Text);
+        Assert.Empty(addEditFeedView.FeedErrorLabel.Text);
+        addEditFeedView.AddEditButton.SafeClick();
+
+        if (!IsWindows) window.MenuButton.SafeClick();
+        menuView = window.MenuView;
+        var lastItem = menuView.FeedNavigationItems[^1];
+        Assert.Equal("FeedTitle", lastItem.TitleLabel.Text);
+
+        cts.Cancel();
+        await serviceTask;
+    }
+}
---
+++ src/NewsReader.UITest/UITest.FeedService/FeedWebApp.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UITest.FeedService;
+
+public static class FeedWebApp
+{
+    public static async Task RunService(SyndicationData feed, string? address, CancellationToken cancellation)
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.Services.AddSingleton(feed);
+        var part = new AssemblyPart(typeof(FeedWebApp).Assembly);
+        builder.Services.AddControllers().ConfigureApplicationPartManager(x => x.ApplicationParts.Add(part));
+
+        var app = builder.Build();
+        app.MapControllers();
+
+        address ??= "http://localhost:5000";
+        var serviceTask = app.RunAsync(address);
+
+        try
+        {
+            await Task.Delay(Timeout.InfiniteTimeSpan, cancellation).ConfigureAwait(false);
+        }
+        catch (OperationCanceledException) { }
+        finally
+        {
+            try
+            {
+                await Task.WhenAll(app.StopAsync(CancellationToken.None), serviceTask).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException) { }
+        }
+    }
+}
