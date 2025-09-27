50e1481f0d457123612cc26e52f434d9acb94e56
jbe2277 <jbe2277@users.noreply.github.com>
2025-09-27 11:24:14 +0200

    NR/UITest: try to improve ContextMenu

--- src/NewsReader.UITest/NewsReader.UITest/Controls/ContextMenu.cs
+++ src/NewsReader.UITest/NewsReader.UITest/Controls/ContextMenu.cs
@@ -4,8 +4,5 @@ namespace UITest.NewsReader.Controls;
 
 public record ContextMenu(AppiumDriver Driver)
 {
-    public AppiumElement Element => Driver.OnPlatform(
-        windows: () => Driver.Find(MobileBy.Name("PopupHost")).Find(MobileBy.ClassName("MenuFlyout")));
-
-    public IReadOnlyList<AppiumElement> MenuItems => Element.FindAll(MobileBy.ClassName("MenuFlyoutItem"), 1);
+    public IReadOnlyList<AppiumElement> MenuItems => Driver.FindAll(MobileBy.ClassName("MenuFlyoutItem"), 1);
 }
