d4b6b4df2b06bc1ff03376210b5e91c59ab9a300
jbe2277 <jbe2277@users.noreply.github.com>
2026-02-28 09:07:16 +0100

    NR/UITest: adapt APK path to net10

--- src/NewsReader.UITest/NewsReader.UITest/UITest.cs
+++ src/NewsReader.UITest/NewsReader.UITest/UITest.cs
@@ -3,7 +3,7 @@
 namespace UITest.NewsReader;
 
 public class UITest() : UITestBase("Waf.NewsReader",        
-        androidApkFile: Environment.GetEnvironmentVariable("UITestApkFile") ?? "src/NewsReader/NewsReader.MauiSystem/bin/Release/net9.0-android/Waf.NewsReader-Signed.apk",
+        androidApkFile: Environment.GetEnvironmentVariable("UITestApkFile") ?? "src/NewsReader/NewsReader.MauiSystem/bin/Release/net10.0-android/Waf.NewsReader-Signed.apk",
         androidAppActivity: "Waf.NewsReader.MainActivity",
         windowsAppId: "Waf.NewsReader_a8txtqew917ny!App",
         Environment.GetEnvironmentVariable("UITestOutputPath") ?? "out/NewsReader.UITest/")
