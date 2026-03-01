695d00d3b3cc35add830ca42a268220842f66c48
jbe2277 <jbe2277@users.noreply.github.com>
2026-03-01 10:48:46 +0100

    Add unit test UnitTestSynchronizationContextUsage

---
+++ src/System.Waf/System.Waf/System.Waf.Core.Test/UnitTesting/UnitTestSynchronizationContextMSTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Waf.UnitTesting;
+
+namespace Test.Waf.UnitTesting
+{
+    [TestClass]
+    public class UnitTestSynchronizationContextMSTest
+    {
+        public UnitTestSynchronizationContext Context { get; private set; } = null!;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Context = UnitTestSynchronizationContext.Create();
+            Assert.AreSame(Context, SynchronizationContext.Current);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Assert.AreSame(Context, SynchronizationContext.Current);
+            Context.Dispose();
+        }
+
+        [TestMethod]
+        public void UnitTestSynchronizationContextUsage()
+        {
+            Assert.AreSame(Context, SynchronizationContext.Current);
+
+            Assert.AreEqual(42, Test1Async().GetResult(Context));
+
+            int secondPartCallCount = 0;
+            Test2Async(() => secondPartCallCount++);
+            Assert.AreEqual(0, secondPartCallCount);
+            Context.WaitFor(() => secondPartCallCount == 1, TimeSpan.FromSeconds(1));
+            Assert.AreEqual(1, secondPartCallCount);
+        }
+
+        private static async Task<int> Test1Async()
+        {
+            await Task.Delay(10);
+            return 42;
+        }
+
+        private static async void Test2Async(Action secondPart)
+        {
+            await Task.Delay(10);
+            secondPart();
+        }
+    }
+}
