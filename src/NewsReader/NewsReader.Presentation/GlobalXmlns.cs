41ecb33ab5449f15f8c4a3650c7810dd27bd4faf
jbe2277 <jbe2277@users.noreply.github.com>
2026-03-08 18:18:50 +0100

    NR: Extend usage of XmlnsDefinition in GlobalXmlns.cs

--- src/NewsReader/NewsReader.Presentation/GlobalXmlns.cs
+++ src/NewsReader/NewsReader.Presentation/GlobalXmlns.cs
@@ -1,2 +1,7 @@
 ﻿[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Waf.NewsReader.Presentation.Converters")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Waf.NewsReader.Presentation.Properties")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Waf.NewsReader.Applications.DataModels", AssemblyName = "Waf.NewsReader.Applications")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Waf.NewsReader.Applications.ViewModels", AssemblyName = "Waf.NewsReader.Applications")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Waf.NewsReader.Domain", AssemblyName = "Waf.NewsReader.Domain")]
